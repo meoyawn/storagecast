a75b71a5ceb98c7880b3dce4d5c3b91190e30596
adelnizamutdinov <stiggpwnz@gmail.com>
2022-01-02 11:42:43 +0300

    preact

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-module.exports = {
+// next.config.js
+const withPreact = require('next-plugin-preact');
+
+module.exports = withPreact({
   webpack: (config, { dev, isServer }) => {
-    if (!dev && isServer) {
+    if (!dev && !isServer) {
       require('./scripts/nextgen')
     }
 
     return config
   }
-}
+});
