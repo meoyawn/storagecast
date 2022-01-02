7258fba4dbb40ed0a0e7e96bd5efb4e2620324b5
adelnizamutdinov <stiggpwnz@gmail.com>
2022-01-02 12:19:08 +0300

    simple

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,31 +1,5 @@
 import React from "react"
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-
-import path from "path"
-import fs from "fs"
-
-class InlineStylesHead extends Head {
-
-  getCssLinks({ sharedFiles }: {
-    sharedFiles: ReadonlyArray<string>
-    pageFiles: ReadonlyArray<string>
-    allFiles: ReadonlyArray<string>
-  }): JSX.Element[] {
-    const { assetPrefix } = this.context
-    return sharedFiles
-      .filter((file) => /\.css$/.test(file))
-      .map((file) => (
-        <style
-          key={file}
-          nonce={this.props.nonce}
-          data-href={`${assetPrefix}/_next/${file}`}
-          dangerouslySetInnerHTML={{
-            __html: fs.readFileSync(path.join(process.cwd(), '.next', file), 'utf-8'),
-          }}
-        />
-      ))
-  }
-}
+import Document, {Head, Html, Main, NextScript} from 'next/document'
 
 // noinspection JSUnusedGlobalSymbols
 export default class MyDocument extends Document {
@@ -33,14 +7,14 @@ export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <InlineStylesHead>
-          <link rel="icon" href="/icon.svg" />
-          <link rel="mask-icon" href="/icon.svg" color="#000" />
-        </InlineStylesHead>
+        <Head>
+          <link rel="icon" href="/icon.svg"/>
+          <link rel="mask-icon" href="/icon.svg" color="#000"/>
+        </Head>
 
         <body>
-          <Main />
-          <NextScript />
+        <Main/>
+        <NextScript/>
         </body>
       </Html>
     )
